79ac9826be094cd83280a6a842fdcb4a197ab12f
FrederickMih <fredtimermih@gmail.com>
2021-09-14 01:18:31 +0100

    Create container and presentation components

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import App from './components/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root') || document.createElement('div'),
+);
