9a99b7d718d3c69750b79177bef3ba9bb9503361
FrederickMih <fredtimermih@gmail.com>
2021-09-14 02:51:37 +0100

    Create Routes component

---
+++ src/routes/routes.js
@@ -0,0 +1,12 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import React from 'react';
+
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route />
+    </Switch>
+  </BrowserRouter>
+);
+
+export default Routes;
