80b9c27d35d0516ba97f7147eb24bb58bdd15643
FrederickMih <fredtimermih@gmail.com>
2021-09-14 02:30:01 +0100

    Add redux thunk middleware

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,11 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import moviesReducer from './moviesReducers';
 import searchLoadingReducer from './searchLoadingReducers';
 import filterMovieReducer from './filterMoviesReducer';
 
-
+const middleware = [thunk];
 const initialState = {};
 
 // eslint-disable-next-line max-len
@@ -13,7 +14,7 @@ export const rootReducer = combineReducers({ movies: moviesReducer, searchLoadin
 const store = createStore(
   rootReducer,
   initialState,
-  composeWithDevTools(applyMiddleware()),
+  composeWithDevTools(applyMiddleware(...middleware)),
 );
 
 export default store;
