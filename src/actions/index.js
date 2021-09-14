30a0e6b9b720649a3d7623205dd1aff74052a452
FrederickMih <fredtimermih@gmail.com>
2021-09-14 01:54:05 +0100

    Create fetchMovies, fetchMorevies, searchMovie, filterMovie, and setLoading actions

---
+++ src/actions/index.js
@@ -0,0 +1,53 @@
+import axios from 'axios';
+
+export const SEARCH_MOVIE = 'SEARCH_MOVIE';
+export const FETCH_MOVIES = 'FETCH_MOVIES';
+export const FETCH_MORE_MOVIES = 'FETCH_MORE_MOVIES';
+export const LOADING = 'LOADING';
+export const FILTER_MOVIE = 'FILTER_MOVIE';
+
+const apiKey = '6959258b';
+
+const fetchMovies = (keyword, page = 1) => (dispatch) => {
+  const req1 = (`https://www.omdbapi.com/?apikey=${apiKey}&s=${keyword}&page=${page}`);
+  axios.get(req1)
+    .then((response) => dispatch({
+      type: FETCH_MOVIES,
+      payload: response.data,
+    }))
+    .catch((err) => (
+      console.log(err)
+    ));
+};
+
+const fetchMoreMovies = (id) => (dispatch) => {
+  const req2 = (`https://www.omdbapi.com/?apikey=${apiKey}&i=${id}`);
+  axios.get(req2)
+    .then((response) => dispatch({
+      type: FETCH_MORE_MOVIES,
+      payload: response.data,
+    }))
+    .catch((err) => (
+      console.log(err)
+    ));
+};
+
+const searchMovie = (keyword) => (dispatch) => {
+  dispatch({
+    type: SEARCH_MOVIE,
+    payload: keyword,
+  });
+};
+
+const setLoading = () => ({
+  type: LOADING,
+});
+
+const filterArticle = (filter) => ({
+  type: FILTER_MOVIE,
+  payload: filter,
+});
+
+export {
+  searchMovie, fetchMovies, fetchMoreMovies, setLoading, filterArticle,
+};
