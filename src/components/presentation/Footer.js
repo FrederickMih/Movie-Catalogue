e138aff5690c248153942fe9d7037119516932a2
FrederickMih <fredtimermih@gmail.com>
2021-09-14 02:45:38 +0100

    Add Footer component

---
+++ src/components/presentation/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../../styles/Footer.css';
+
+const Footer = () => (
+  <footer className="footer">
+    Copyright
+    {' '}
+    &copy;
+    {' '}
+    <strong>Mih Frederick</strong>
+  </footer>
+);
+
+export default Footer;
