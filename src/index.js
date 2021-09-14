ac990f6f8000c70708cbd56803e24797e665d7f3
FrederickMih <fredtimermih@gmail.com>
2021-09-14 03:00:38 +0100

    Add react-router-dom package

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './components/App';
+import App from './components/presentation/App';
 
 ReactDOM.render(
   <React.StrictMode>
