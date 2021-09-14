e1d54c89a28e046dc9ed07053bd9d69dba0d439f
FrederickMih <fredtimermih@gmail.com>
2021-09-14 02:41:26 +0100

    Add Navbar component

---
+++ src/components/presentation/App.js
@@ -0,0 +1,7 @@
+const App = () => (
+  <div className="app-div">
+    <h1>catalogue</h1>
+  </div>
+);
+
+export default App;
---
+++ src/components/presentation/NavBar.js
@@ -0,0 +1,9 @@
+import '../styles/NavBar.css';
+
+const Navbar = () => (
+  <nav className="navbar">
+    <h1 className="nav-title">Movies Catalogue</h1>
+  </nav>
+);
+
+export default Navbar;
