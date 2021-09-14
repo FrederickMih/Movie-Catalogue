e679c919509c6286951d52cd82d440d3093edc0a
FrederickMih <fredtimermih@gmail.com>
2021-09-14 02:25:27 +0100

    Add redux-devtools-extension package

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,9 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import moviesReducer from './moviesReducers';
 import searchLoadingReducer from './searchLoadingReducers';
 import filterMovieReducer from './filterMoviesReducer';
+
 
 const initialState = {};
 
@@ -11,6 +13,7 @@ export const rootReducer = combineReducers({ movies: moviesReducer, searchLoadin
 const store = createStore(
   rootReducer,
   initialState,
+  composeWithDevTools(applyMiddleware()),
 );
 
 export default store;
