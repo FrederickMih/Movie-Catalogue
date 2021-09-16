cfc267b4abbc40a5393f15ddd263ef12be7d3ae5
FrederickMih <fredtimermih@gmail.com>
2021-09-16 10:05:39 +0100

    Add test to render App component correctly

---
+++ src/_tests_/components/App.test.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import PropTypes from 'prop-types';
+import { rootReducer } from '../../reducers/index';
+import App from '../../components/presentation/App';
+
+const state = {};
+const middleware = [thunk];
+const store = createStore(rootReducer, state, composeWithDevTools(applyMiddleware(...middleware)));
+const Wrapper = ({ children }) => (
+  <Provider store={store}>{children}</Provider>
+);
+
+Wrapper.propTypes = {
+  children: PropTypes.shape({}).isRequired,
+};
+
+describe('App', () => {
+  it('renders correctly', () => {
+    const tree = render(<App />, { wrapper: Wrapper });
+    expect(tree).toMatchSnapshot();
+  });
+});
