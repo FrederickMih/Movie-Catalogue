14e3dcc8b7f48e4f089786cb6bcb00708e04bbe0
FrederickMih <fredtimermih@gmail.com>
2021-09-16 10:59:28 +0100

    Add test that allows user to filter movies my genre 'movies'

--- src/_tests_/reducers/filterMoviesReducer.test.js
+++ src/_tests_/reducers/filterMoviesReducer.test.js
@@ -1,7 +1,18 @@
-import { initialState } from '../../reducers/filterMoviesReducer';
+import filterMovieReducer, { initialState } from '../../reducers/filterMoviesReducer';
 
 test('It should filter All movies initially', () => {
   expect(initialState).toEqual({
     filter: 'All',
+  });
+});
+
+test('Should allow user to filter movies my categories ', () => {
+  const action = {
+    type: 'FILTER_MOVIE',
+    payload: 'movies',
+  };
+  expect(filterMovieReducer(initialState, action)).toEqual({
+    ...initialState,
+    filter: 'movies',
   });
 });
