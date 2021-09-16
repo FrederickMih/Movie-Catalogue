f70c1770a0ec80149b5a2bb78cb99381a1b2fbdb
FrederickMih <fredtimermih@gmail.com>
2021-09-16 11:30:33 +0100

    Ad test not to fetch movies when wrong action is given

--- src/_tests_/reducers/moviesReducer.test.js
+++ src/_tests_/reducers/moviesReducer.test.js
@@ -21,3 +21,29 @@ test('moviesReducers does not load when wrong action is supplied', () => {
     loading: action.payload,
   });
 });
+
+test('It should not fetch movies of the Home page when wrong action is given ', () => {
+  const action = {
+    type: 'WRONG_ACTION_AGAIN',
+    payload: ' movies',
+    loading: false,
+  };
+  expect(moviesReducers(initialState, action)).not.toEqual({
+    ...initialState,
+    movie: action.payload,
+    loading: false,
+  });
+});
+
+test('I should not fetch data of the detail page when wrong action is supplied ', () => {
+  const action = {
+    type: 'WRONG_ACTION',
+    movies: 'more movies',
+    loading: false,
+  };
+  expect(moviesReducers(initialState, action)).not.toEqual({
+    ...initialState,
+    movies: action.movie,
+    loading: false,
+  });
+});
