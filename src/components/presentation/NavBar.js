68bfc3d11ed94b2acf5b16d77c964c8770f3660a
FrederickMih <fredtimermih@gmail.com>
2021-09-16 10:24:09 +0100

    Add test file to render the Navbar component correctly

--- src/components/presentation/NavBar.js
+++ src/components/presentation/NavBar.js
@@ -1,11 +1,8 @@
 import '../styles/NavBar.css';
-// import SearchForm from '../container/SearchForm';
 
 const Navbar = () => (
   <nav className="navbar">
     <h1 className="nav-title">Movies Catalogue</h1>
-    {/* <SearchForm /> */}
-
   </nav>
 );
 
